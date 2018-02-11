987738023f2ab4b925e8c7d86fb441c2dcf8164d
sebastian correa <scorrea1@wsgc.com>
2018-02-11 18:51:22 -0500

    Enforce a null check with composable code branching using Either

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
 const Box = x =>
-({
-  map: f => Box(f(x)),
-  fold: f => f(x),
-  inspect: function() {
-    console.log(`Box(${x})`);   
-    return this; 
-    } 
-})
+    ({
+        map: f => Box(f(x)),
+        fold: f => f(x),
+        inspect: function () {
+            console.log(`Box(${x})`);
+            return this;
+        }
+    })
+const Right = x =>
+    ({
+        map: f => Right(f(x)),
+        fold: (f, g) => g(x),
+        inspect: function () {
+            console.log(`Right(${x})`);
+            return this;
+        }
+    })
 
-const moneyToFloat = str =>
-  Box(str)
-  .map(s => s.replace(/\$/g, ''))
-  .map(r => parseFloat(r))
+const Left = x =>
+    ({
+        map: f => Left(x),
+        fold: (f, g) => f(x),
+        inspect: function () {
+            console.log(`Left(${x})`);
+            return this;
+        }
+    })
 
-const percentToFloat = str =>
-  Box(str.replace(/\%/g, ''))
-  .map(replaced => parseFloat(replaced))
-  .map(number => number * 0.01)
+const fromNullable = x =>
+    x != null ? Right(x) : Left(null)
 
-  
-const applyDiscount = (price, discount) =>
-    moneyToFloat(price)
+const findColor = name =>
+    fromNullable({
+        red: '#ff4444',
+        blue: '#3b5998',
+        yellow: '#fff68f'
+    }[name])
+
+const resGood = findColor('red')
     .inspect()
-    .fold(cost =>
-        percentToFloat(discount)
-        .inspect()
-        .fold(savings =>
-            cost - cost * savings)
+    .map(c => c.slice(1))
+    .inspect()
+    .map(c => c.toUpperCase())
+    .inspect()
+    .fold(e => 'no color',
+        x => x
     )
 
-const result = applyDiscount('$5.00', '20%')
-console.log(result)
+console.log(resGood)
+
+const resBad = findColor('notexistColorXSXSXS')
+    .inspect()
+    .map(c => c.slice(1))
+    .inspect()
+    .map(c => c.toUpperCase())
+    .inspect()
+    .fold(e => 'no color',
+        x => x
+    )
+
+console.log(resBad)
