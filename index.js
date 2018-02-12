b1b54297328d0c5b9fef08e8246efbfd2972602d
sebastian correa <scorrea1@wsgc.com>
2018-02-11 20:28:53 -0500

    semigroups example

--- index.js
+++ index.js
@@ -1,47 +1,4 @@
-const Box = x =>
-    ({
-        map: f => Box(f(x)),
-        fold: f => f(x),
-        inspect: function () {
-            console.log(`Box(${x})`);
-            return this;
-        }
-    })
-const Right = x =>
-    ({
-        chain: f => f(x),
-        map: f => Right(f(x)),
-        fold: (f, g) => g(x),
-        inspect: function () {
-            console.log(`Right(${x})`);
-            return this;
-        }
-    })
-
-const Left = x =>
-    ({
-        chain: f => f(x),
-        map: f => Left(x),
-        fold: (f, g) => f(x),
-        inspect: function () {
-            console.log(`Left(${x})`);
-            return this;
-        }
-    })
-
-const fromNullable = x =>
-    x != null ? Right(x) : Left(null)
-
-
-const tryCatch = f => {
-    try {
-        return Right(f())
-    } catch (e) {
-        return Left(e)
-    }
-}
-
-/////
+const {Map:IMap} = require('immutable');
 
 const Sum = x =>
 ({
@@ -49,15 +6,8 @@ const Sum = x =>
   concat: ({x: y}) =>
     Sum(x + y),
   inspect: () =>
-    `Sum(${x})`,
-  toString: () =>
     `Sum(${x})`
 })
-
-const resSum = Sum(1).concat(Sum(2))
-
-console.log(resSum);
-
 
 const All = x =>
 ({
@@ -65,24 +15,22 @@ const All = x =>
   concat: ({x: y}) =>
     All(x && y),
   inspect: () =>
-    `All(${x})`,
-  toString: () =>
     `All(${x})`
 })
 
-const resAll = All(false).concat(All(true))
-
-console.log(resAll);
-
 const First = x =>
 ({
+  x,
   concat: _ =>
     First(x),
   inspect: () =>
-    `First(${x})`,
-  toString: () =>
     `First(${x})`
 })
 
-const res = First("blah").concat(First("ice cream")).concat(First('meta programming'))
-console.log(res)
+const acct1 = IMap({ name: First('Nico'), isPaid: All(true), points: Sum(10), friends: ['Franklin'] })
+
+const acct2 = IMap({ name: First('Nico'), isPaid: All(false), points: Sum(2), friends: ['Gatsby'] })
+
+const res = acct1.concat(acct2)
+
+console.log(res.toJS());
